403b1fbbd0cd7f4f964db667f820d0d395386c58
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2019-11-27 21:13:55 +0100

    Update ZSH configuration.

---
+++ archlinux/.config/zsh/plugins.sh
@@ -0,0 +1,8 @@
+source /home/phaazon/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/phaazon/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/phaazon/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/phaazon/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /home/phaazon/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Tarrasch-SLASH-zsh-bd/bd.plugin.zsh
+fpath+=( /home/phaazon/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Tarrasch-SLASH-zsh-bd )
+source /home/phaazon/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-hschne-SLASH-fzf-git/fzf-git.plugin.zsh
+fpath+=( /home/phaazon/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-hschne-SLASH-fzf-git )
--- archlinux/.zshrc
+++ archlinux/.zshrc
@@ -22,8 +22,13 @@ zstyle ':completion::complete:*' cache-path $ZSH_CACHE_DIR
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 
+# History
+HISTFILE=~/.zsh_history
+HISTSIZE=1000
+SAVEHIST=1000
+setopt appendhistory
+
 # Plugins
-source <(antibody init)
 source ~/.config/zsh/plugins.sh
 
 export NVM_DIR="$HOME/.nvm"
