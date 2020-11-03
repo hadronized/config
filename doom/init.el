f320888c0bff4217455e2ed60d4a2ce1bfc9482c
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2020-11-03 23:26:04 +0100

    Update.

--- doom/init.el
+++ doom/init.el
@@ -23,7 +23,7 @@
        company           ; the ultimate code completion backend
        ;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +fuzzy +icons)              ; a search engine for love and life
+       (ivy +fuzzy +icons +prescient)              ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
