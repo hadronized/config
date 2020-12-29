2ee54d029e8588526b2fa5f574be6117ebb83987
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2020-12-29 11:16:33 +0100

    Small emacs update to remove / add small features.

--- doom/init.el
+++ doom/init.el
@@ -146,8 +146,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +journal
-            +dragndrop
+       (org +dragndrop
             +pandoc
             +pretty)              ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
