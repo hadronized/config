3d638847fdf20be2123fa205e9ad490bbe4f7f7d
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2021-04-09 00:04:32 +0200

    Fix colorschemes in Neovim.

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -1,7 +1,19 @@
 local M = {}
 
+-- Setup a colorscheme.
+--
+-- The `colorscheme` argument is the name of the colorscheme to switch to. If non nil, it will switch to the
+-- colorescheme and install autocommands to override its highlights.
 function M.setup(colorscheme)
-  vim.api.nvim_exec('colorscheme ' .. colorscheme, false)
+  if colorscheme ~= nil then
+    vim.api.nvim_command('colorscheme ' .. colorscheme)
+
+    vim.api.nvim_command([[augroup EdgeCustom]])
+    vim.api.nvim_command([[  autocmd!]])
+    vim.api.nvim_command(string.format("  autocmd ColorScheme %s lua require'config.colorscheme'.setup()", colorscheme))
+    vim.api.nvim_command([[augroup END]])
+  end
+
   vim.api.nvim_exec([[
     hi! link markdownLinkTextDelimiter NonText
     hi! link markdownLinkDelimiter NonText
@@ -29,9 +41,13 @@ function M.setup(colorscheme)
     hi! markdownListMarker guifg=#ff6c6b
     hi! htmlItalic guifg=#a9a1e1 gui=italic
     hi! htmlBold guifg=#da8548 gui=bold
+    hi! link LspDiagnosticsVirtualTextHint LspDiagnosticsSignHint
+    hi! link LspDiagnosticsVirtualTextInformation LspDiagnosticsSignInformation
+    hi! link LspDiagnosticsVirtualTextWarning LspDiagnosticsSignWarning
+    hi! link LspDiagnosticsVirtualTextError LspDiagnosticsSignError
   ]], false)
 end
 
-M.setup('doom-one')
+M.setup('edge')
 
 return M
