092a312eaf11d48732c83440b4f6d67bed87095b
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2021-04-30 11:58:26 +0200

    Update the Doom config.

--- doom/config.el
+++ doom/config.el
@@ -1,102 +1,174 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "Dimitri Sabadie"
-      user-mail-address "dimitri.sabadie@gmail.com")
-
-;; Various general configuration.
-(setq-default tab-width 2)
-(setq-default tab-always-indent nil)
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 12))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
+;; The best theme — ever.
 (setq doom-theme 'doom-one)
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+;; Default fill-column to 120 and add the keybinding to toggle it on and off.
+(setq-default fill-column 120)
+(map! :leader
+      :desc "Auto fill"
+      :n "t a" 'auto-fill-mode)
 
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+;; Use , as local leader.
+(setq doom-localleader-key ",")
 
-;; yasnippet
-(setq +lsp-company-backends '(company-capf :with company-yasnippet))
+;; Font setting.
+(setq doom-font (font-spec :family "JetBrainsMono" :size 16 :weight 'light)
+      doom-variable-pitch-font (font-spec :family "Noto Serif" :size 17)
+      ivy-posframe-font (font-spec :family "JetBrainsMono" :size 19))
 
-;; org-mode
-(setq org-directory "~/org")
-(setq org-noter-notes-search-path '("~/org/notes/"))
+;; Set all-the-icons factor to 1.0 to prevent weird issues with the modeline.
+(setq all-the-icons-scale-factor 1.0)
 
-;; rgb
-(add-hook! 'rainbow-mode-hook
-  (hl-line-mode (if rainbow-mode -1 +1)))
+;; Prevents some cases of Emacs flickering
+(add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
 
-;; yasnippets
-(setq yasnippet-snippets-dir "~/.config/doom/snippets")
+;; Undo / redo.
+(map! :n "U" 'undo-tree-redo)
 
-;; keybindings dela muerte
-;; standard stuff dela muerte
-(map! :nv :desc "Redo" "U" #'undo-fu-only-redo)
+(map! :nvom "à" 'evil-snipe-t)
+(map! :nvom "À" 'evil-snipe-T)
 
-;; window remapping
+;; Fix for macOS and its stupid support for alt-gr.
+(when (string-equal system-type "darwin")
+  (progn
+    (setq mac-option-key-is-meta nil)
+    (setq mac-command-key-is-meta t)
+    (setq mac-command-modifier 'meta)
+    (setq mac-option-modifier nil)))
+
+(setq evil-shift-width 2)
+
+(add-hook! 'before-save-hook 'delete-trailing-whitespace)
+
 (map! :n "w o" #'evil-window-split)
 
-;; avy dela muerte
+(setq auto-save-default nil)
+
+(add-hook! 'rustic-mode-hook (auto-fill-mode 1))
+
 (map! :leader
-      :desc "avy words" "w" #'avy-goto-word-0
-      :desc "avy lines" "l" #'avy-goto-line
-      :desc "avy char" "é" #'avy-goto-char)
+      :prefix "b"
+      :desc "Rename buffer"
+      :n "R" #'rename-buffer)
 
-;; altgr configuration
-(setq mac-option-key-is-meta nil)
-(setq mac-command-key-is-meta t)
-(setq mac-command-modifier 'meta)
-(setq mac-option-modifier nil)
+(map! :nvom "è" 'evil-avy-goto-word-0)
 
-;; ignore override (dela mierda)
-(after! (markdown-mode evil-markdown)
-  (define-key markdown-mode-map (kbd "<S-tab>") nil)
-  (define-key markdown-mode-map (kbd "<S-iso-lefttab>") nil)
-  (define-key markdown-mode-map (kbd "<backtab>") nil)
-  (define-key evil-markdown-mode-map (kbd "<S-tab>") nil)
-  (define-key evil-markdown-mode-map (kbd "<S-iso-lefttab>") nil)
-  (define-key evil-markdown-mode-map :nv (kbd "<backtab>") nil))
+(after! avy
+  (setq avy-keys '(?e ?t ?o ?v ?x ?q ?p ?d ?y ?g ?f ?b ?l ?z ?h ?c ?k ?i ?s ?u ?r ?a ?n)
+        avy-background t))
 
-(map! :after org
-      :map evil-org-mode-map
-      :n "M-t" #'org-metadown
-      :n "M-s" #'org-metaup)
+(setq avy-orders-alist
+      '((avy-goto-char . avy-order-closest)
+        (avy-goto-word-0 . avy-order-closest)))
 
-;; markdown-mode
-(after! markdown-mode
-  (setq markdown-list-indent-width 2))
+(setq flycheck-check-syntax-automatically '(save idle-change))
+
+(add-hook 'after-init-hook 'global-company-mode)
+
+(after! sh-script
+  (set-company-backend! 'shmode
+    '(company-shell :with company-yasnippet)))
+
+;; ;; OVERRIDES
+;; (map! :mode emacs :map company-active-map
+;;       "<RET>" nil
+;;       "<return>" nil)
+
+;; (map! :mode emacs :map general-company-active-map-backup-map
+;;       "<RET>" nil
+;;       "<return>" nil)
+
+;; ;; KEYBINDINGS
+;; (map! :map company-mode-map
+;;       :i "C-y" 'company-complete-selection)
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+(setq org-directory "~/org")
+(setq org-agenda-files '("~/org"))
+
+;; (setq org-modules '(ol-bibtex habits))
+
+(map! :map org-mode-map
+      :n "C" 'org-shiftleft
+      :n "R" 'org-shiftright
+      :n "T" 'org-shiftdown
+      :n "S" 'org-shiftup
+      :n "M-c" 'org-metaleft
+      :n "M-r" 'org-metaright
+      :n "M-s" 'org-metaup
+      :n "M-t" 'org-metadown
+      :n "M-S-c" 'org-do-promote
+      :n "M-S-r" 'org-do-demote)
+
+(setq org-link-abbrev-alist
+      '(("google"    . "http://www.google.com/search?q=%s")
+        ("gmap" . "https://maps.google.com/maps?q=%s")
+        ("gimages" . "https://google.com/images?q=%s")
+        ("doom-repo" . "https://github.com/hlissner/doom-emacs/%s")
+        ("wolfram" . "https://wolframalpha.com/input/?i=%s")
+        ("wikipedia" . "https://en.wikipedia.org/wiki/%s")
+        ("duckduckgo" . "https://duckduckgo.com/?q=%s")
+        ("youtube" . "https://youtube.com/watch?v=%s")
+        ("github" . "https://github.com/%s")
+        ("wr" . "https://www.wordreference.com/enfr/%s")))
+
+;; (setq org-todo-keywords
+;;       '((sequence "TODO(t)" "WIP(w!/!)" "FROZEN(z!/!)" "LATER(l!/!)" "|" "DONE(d!/!)")
+;;         (sequence "BUG(b)" "|" "FIXED(f!/!)")
+;;         (sequence "FEATURE(r)" "|" "IMPLEMENTED(i!/!)")
+;;         (sequence "|" "CANCELLED(c@)")))
+
+;; (setq org-todo-keyword-faces
+;;       '(("TODO" . "blue") ("WIP" . (:foreground "green" :weight bold)) ("FROZEN" . "blue") ("DONE" . "grey")
+;;         ("BUG" . (:foreground "red" :weight bold)) ("FIXED" (:foreground "blue" :weight bold))
+;;         ("FEATURE" . "purple") ("IMPLEMENTED" . (:foreground "blue" :weight bold))
+;;         ("CANCELLED" . "grey")))
+
+(setq org-log-done 'time)
+
+(setq org-log-into-drawer t)
+
+(setq org-fancy-priorities-list '("❗" "⬆" "⬇"))
+
+(defun org-summary-todo (n-done n-not-done)
+  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  (let (org-log-done org-log-states)   ; turn off logging
+    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+
+(add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
+
+(setq org-tag-alist '(("spare" . ?s) ("work" . ?w) ("learn" . ?l)))
+
+(after! git-gutter-fringe
+  (if (fboundp 'fringe-mode) (fringe-mode '4))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [255 255 255]
+    nil nil 'bottom))
+
+(setq haskell-process-type 'cabal-new-repl)
+
+(setq rustic-indent-offset 2)
+
+(use-package! tree-sitter
+  :config
+  (require 'tree-sitter-langs)
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(setq projectile-project-search-path "~/dev")
+
+(setq-default treemacs-show-cursor t)
+
+(map! :leader
+      :prefix "c"
+      :desc "Symbol outline"
+      :n "o" #'lsp-treemacs-symbols)
+
+(map! :leader
+      :prefix "c"
+      :desc "List errors"
+      :n "x" #'lsp-treemacs-errors-list)
+
+(setq epa-file-encrypt-to '(9BFDD250BBECA905))
+
+(add-hook! 'rustic-mode-hook (modify-syntax-entry ?_ "w"))
