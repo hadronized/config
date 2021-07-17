d02518b8cd0dba4e0768d485cc4be14844f96aff
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2021-07-17 03:17:09 +0200

    Neovim and bspwm update.

--- nvim/lua/config/pkg.lua
+++ nvim/lua/config/pkg.lua
@@ -145,8 +145,7 @@ require('packer').startup(function(use)
   use 'nvim-treesitter/nvim-treesitter'
 
   use {
-    -- 'phaazon/hop.nvim',
-    '~/dev/hop.nvim',
+    'phaazon/hop.nvim',
     as = 'hop',
     config = function()
       require'hop'.setup {
@@ -220,26 +219,26 @@ require('packer').startup(function(use)
       local tree_cb = require'nvim-tree.config'.nvim_tree_callback
       vim.g.nvim_tree_bindings = {
         -- default mappings
-        { key = "<CR>",   cb = tree_cb("edit") },
-        { key = "o",      cb = tree_cb("cd") },
-        { key = "<C-v>",  cb = tree_cb("vsplit") },
-        { key = "<C-o>",  cb = tree_cb("split") },
-        { key = "<C-t>",  cb = tree_cb("tabnew") },
-        { key = "<BS>",   cb = tree_cb("close_node") },
-        { key = "<S-CR>", cb = tree_cb("close_node") },
-        { key = "<Tab>",  cb = tree_cb("preview") },
-        { key = "I",      cb = tree_cb("toggle_ignored") },
-        { key = ".",      cb = tree_cb("toggle_dotfiles") },
-        { key = "R",      cb = tree_cb("refresh") },
-        { key = "a",      cb = tree_cb("create") },
-        { key = "d",      cb = tree_cb("remove") },
-        { key = "r",      cb = tree_cb("rename") },
-        { key = "<C-r>",  cb = tree_cb("full_rename") },
-        { key = "x",      cb = tree_cb("cut") },
-        { key = "y",      cb = tree_cb("copy") },
-        { key = "p",      cb = tree_cb("paste") },
-        { key = "c",      cb = tree_cb("dir_up") },
-        { key = "q",      cb = tree_cb("close") },
+        { key = { "<CR>" },   cb = tree_cb("edit") },
+        { key = { "o" },      cb = tree_cb("cd") },
+        { key = { "<C-v>" },  cb = tree_cb("vsplit") },
+        { key = { "<C-o>" },  cb = tree_cb("split") },
+        { key = { "<C-t>" },  cb = tree_cb("tabnew") },
+        { key = { "<BS>" },   cb = tree_cb("close_node") },
+        { key = { "<S-CR>" }, cb = tree_cb("close_node") },
+        { key = { "<Tab>" },  cb = tree_cb("preview") },
+        { key = { "I" },      cb = tree_cb("toggle_ignored") },
+        { key = { "." },      cb = tree_cb("toggle_dotfiles") },
+        { key = { "R" },      cb = tree_cb("refresh") },
+        { key = { "a" },      cb = tree_cb("create") },
+        { key = { "d" },      cb = tree_cb("remove") },
+        { key = { "r" },      cb = tree_cb("rename") },
+        { key = { "<C-r>" },  cb = tree_cb("full_rename") },
+        { key = { "x" },      cb = tree_cb("cut") },
+        { key = { "y" },      cb = tree_cb("copy") },
+        { key = { "p" },      cb = tree_cb("paste") },
+        { key = { "c" },      cb = tree_cb("dir_up") },
+        { key = { "q" },      cb = tree_cb("close") },
       }
     end
   }
@@ -252,7 +251,7 @@ require('packer').startup(function(use)
 
   use {
     'TimUntersberger/neogit',
-    disable = true
+    disable = false,
   }
 
   use {
