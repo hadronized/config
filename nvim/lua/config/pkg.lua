682be25c617dc4fa65cf39eee2ba0df9e33cc2d4
Dimitri Sabadie <dimitri.sabadie@datadoghq.com>
2021-10-06 11:20:59 +0200

    Update Neovim configuration.

--- nvim/lua/config/pkg.lua
+++ nvim/lua/config/pkg.lua
@@ -1,15 +1,10 @@
 vim.g.nvim_tree_side = 'left'
 vim.g.nvim_tree_width = 40
 vim.g.nvim_tree_ignore = { '.git', '.target' }
-vim.g.nvim_tree_auto_open = 0
-vim.g.nvim_tree_auto_close = 1
-vim.g.nvim_tree_quit_on_open = 0
-vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_hide_dotfiles = 0
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_root_folder_modifier = ':.'
-vim.g.nvim_tree_tab_open = 0
 vim.g.nvim_tree_allow_resize = 1
 vim.g.nvim_tree_show_icons = {
   git = 1,
@@ -31,8 +26,6 @@ vim.g.nvim_tree_icons = {
     open = ''
   },
 }
-
-vim.g.nvim_tree_disable_default_keybindings = 1
 
 vim.g.material_style = 'palenight'
 vim.g.material_italic_keywords = true
@@ -72,8 +65,6 @@ require('packer').startup(function(use)
   use 'embark-theme/vim'
 
   use 'bkegley/gloombuddy'
-
-  use 'sainnhe/edge'
 
   use {
     'joshdick/onedark.vim',
@@ -175,41 +166,47 @@ require('packer').startup(function(use)
     as = 'nvim-tree',
     config = function()
       local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-      vim.g.nvim_tree_bindings = {
-        -- default mappings
-        { key = { "<CR>", "<TAB>" },   cb = tree_cb("edit") },
-        { key = { "o" },      cb = tree_cb("cd") },
-        { key = { "<C-v>" },  cb = tree_cb("vsplit") },
-        { key = { "<C-o>" },  cb = tree_cb("split") },
-        { key = { "<C-t>" },  cb = tree_cb("tabnew") },
-        { key = { "<BS>" },   cb = tree_cb("close_node") },
-        { key = { "<S-CR>" }, cb = tree_cb("close_node") },
-        { key = { "<Tab>" },  cb = tree_cb("preview") },
-        { key = { "I" },      cb = tree_cb("toggle_ignored") },
-        { key = { "." },      cb = tree_cb("toggle_dotfiles") },
-        { key = { "R" },      cb = tree_cb("refresh") },
-        { key = { "a" },      cb = tree_cb("create") },
-        { key = { "d" },      cb = tree_cb("remove") },
-        { key = { "r" },      cb = tree_cb("rename") },
-        { key = { "<C-r>" },  cb = tree_cb("full_rename") },
-        { key = { "x" },      cb = tree_cb("cut") },
-        { key = { "y" },      cb = tree_cb("copy") },
-        { key = { "p" },      cb = tree_cb("paste") },
-        { key = { "c" },      cb = tree_cb("dir_up") },
-        { key = { "q" },      cb = tree_cb("close") },
+
+      require'nvim-tree'.setup {
+        update_to_buf_dir = {
+          auto_open = false,
+          auto_close = true,
+        },
+        tab_open = false,
+        follow = true,
+        view = {
+          mappings = {
+            custom_only = true,
+            list = {
+              -- default mappings
+              { key = { "<CR>", "<TAB>" },   cb = tree_cb("edit") },
+              { key = { "o" },      cb = tree_cb("cd") },
+              { key = { "<C-v>" },  cb = tree_cb("vsplit") },
+              { key = { "<C-o>" },  cb = tree_cb("split") },
+              { key = { "<C-t>" },  cb = tree_cb("tabnew") },
+              { key = { "<BS>" },   cb = tree_cb("close_node") },
+              { key = { "<S-CR>" }, cb = tree_cb("close_node") },
+              { key = { "<Tab>" },  cb = tree_cb("preview") },
+              { key = { "I" },      cb = tree_cb("toggle_ignored") },
+              { key = { "." },      cb = tree_cb("toggle_dotfiles") },
+              { key = { "R" },      cb = tree_cb("refresh") },
+              { key = { "a" },      cb = tree_cb("create") },
+              { key = { "d" },      cb = tree_cb("remove") },
+              { key = { "r" },      cb = tree_cb("rename") },
+              { key = { "<C-r>" },  cb = tree_cb("full_rename") },
+              { key = { "x" },      cb = tree_cb("cut") },
+              { key = { "y" },      cb = tree_cb("copy") },
+              { key = { "p" },      cb = tree_cb("paste") },
+              { key = { "c" },      cb = tree_cb("dir_up") },
+              { key = { "q" },      cb = tree_cb("close") },
+            },
+          }
+        }
       }
     end
   }
 
   use 'nvim-lua/plenary.nvim'
-
-  use {
-    'TimUntersberger/neogit',
-    requires = 'nvim-lua/plenary.nvim',
-    config = function()
-      require'neogit'.setup {}
-    end
-  }
 
   use 'neovim/nvim-lspconfig'
 
