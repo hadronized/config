61b25a7e2f0e0afe7d359020e6519170c2b0a22a
Dimitri Sabadie <dimitri.sabadie@datadoghq.com>
2022-06-01 15:40:40 +0200

    Neovim update.

--- nvim/lua/config/pkg.lua
+++ nvim/lua/config/pkg.lua
@@ -406,11 +406,11 @@ require('packer').startup(function(use)
     config = function()
       require('gitsigns').setup({
         signs = {
-          add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-          change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-          delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+          add          = {hl = 'GitSignsAdd'   , text = '▍', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+          change       = {hl = 'GitSignsChange', text = '▍', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+          delete       = {hl = 'GitSignsDelete', text = '▂', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
           topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-          changedelete = {hl = 'GitSignsChange', text = '│ ', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+          changedelete = {hl = 'GitSignsChange', text = '▍', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
         },
         keymaps = {
           noremap = true,
