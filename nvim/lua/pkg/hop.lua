583a398a4933937b6daa5a94fa74c4c05fc26a11
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2022-07-26 16:32:07 +0200

    More Hop motions.

--- nvim/lua/pkg/hop.lua
+++ nvim/lua/pkg/hop.lua
@@ -9,39 +9,59 @@ return {
     [''] = {
       {
         key = 'è',
-        cmd = 'HopWord'
+        cmd = 'HopWord',
       },
       {
         key = 'È',
-        cmd = 'HopChar1'
+        cmd = 'HopWordMW',
       },
       {
-        key = '<C-l>',
-        cmd = 'HopLineStart'
+        key = '<leader>he',
+        lua = "require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END, hint_offset = 1})",
       },
       {
-        key = '<leader>/',
-        cmd = 'HopPattern'
+        key = '<leader>hE',
+        lua = "require'hop'.hint_words({ hint_position = require'hop.hint'.HintPosition.END, hint_offset = 1, mult_windows = true})",
       },
       {
-        key = '<C-e>',
-        cmd = 'HopWordMW'
+        key = '<leader>hl',
+        cmd = 'HopLineStart',
+      },
+      {
+        key = '<leader>hL',
+        cmd = 'HopLineStartMW',
+      },
+      {
+        key = '<leader>hs',
+        cmd = 'HopPattern',
+      },
+      {
+        key = '<leader>hS',
+        cmd = 'HopPatternMW',
+      },
+      {
+        key = '<leader>hc',
+        cmd = 'HopChar1',
+      },
+      {
+        key = '<leader>hC',
+        cmd = 'HopChar1MW',
       },
       {
         key = 'f',
-        lua = "require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })"
+        lua = "require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })",
       },
       {
         key = 'F',
-        lua = "require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })"
+        lua = "require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })",
       },
       {
         key = 'à',
-        lua = "require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })"
+        lua = "require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })",
       },
       {
         key = 'À',
-        lua = "require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = -1 })"
+        lua = "require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = -1 })",
       },
     },
   }
