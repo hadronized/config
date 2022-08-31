2d023a7d63e0fcbae77b9567088d79e0374c2be8
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2022-08-31 10:52:56 +0200

    Neovim update.

--- nvim/lua/pkg/mind.lua
+++ nvim/lua/pkg/mind.lua
@@ -11,6 +11,26 @@ return {
       ui = {
         width = 40,
       },
+
+      keymaps = {
+        normal = {
+          T = function(args)
+            require'mind.ui'.with_cursor(function(line)
+              local tree = args.get_tree()
+              local node = require'mind.node'.get_node_by_line(tree, line)
+
+              if node.icon == nil or node.icon == ' ' then
+                node.icon = ' '
+              elseif node.icon == ' ' then
+                node.icon = ' '
+              end
+
+              args.save_tree()
+              require'mind.ui'.rerender(tree, args.opts)
+            end)
+          end,
+        }
+      }
     }
   end,
   keybindings = {
